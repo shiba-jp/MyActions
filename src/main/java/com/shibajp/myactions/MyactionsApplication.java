bf7db53aad398e1a94135c1c00992291295c8cb8
shiba-jp <shibazaki.yuhei@gmail.com>
2022-06-18 21:22:46 +0900

    first commit

---
+++ src/main/java/com/shibajp/myactions/MyactionsApplication.java
@@ -0,0 +1,13 @@
+package com.shibajp.myactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shibajp/myactions/MyactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shibajp.myactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
